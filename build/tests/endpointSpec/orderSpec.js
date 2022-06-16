915e451785dd35683653a06c6621b8246c7a0c13
HossamGouda <hos.gouda90@gmail.com>
2022-06-16 18:36:40 +0300

    general updates for unite testing

--- build/tests/endpointSpec/orderSpec.js
+++ build/tests/endpointSpec/orderSpec.js
@@ -1,102 +1,213 @@
 "use strict";
-// import supertest from 'supertest';
-// import db from '../../database/database';
-// import app from '../../server';
-// import UserModel from '../../models/user.model';
-// import User from '../../types/user.types';
-// const userModel = new UserModel();
-// const request = supertest(app);
-// // eslint-disable-next-line @typescript-eslint/no-inferrable-types
-// let token: string = '';
-// describe('Test Orders API Endpoints', () => {
-//   beforeAll(async () => {
-//     const user = {
-//       email: 'gouda@h.com',
-//       user_name: 'hos',
-//       first_name: 'Hossam',
-//       last_name: 'Gouda',
-//       password: '321',
-//     } as User;
-//     await userModel.create(user);
-//   });
-//   afterAll(async () => {
-//     // clean db
-//     const connection = await db.connect();
-//     const sql =
-//       'DELETE FROM orders;\nALTER SEQUENCE orders_id_seq RESTART WITH 1;\n DELETE FROM users;\nALTER SEQUENCE users_id_seq RESTART WITH 1;';
-//     await connection.query(sql);
-//     connection.release();
-//   });
-//   describe('Test Authenticate method', () => {
-//     it('should be able to authenticate to get token', async () => {
-//       const res = await request
-//         .post('/api/users/auth')
-//         .set('Content-type', 'application/json')
-//         .set('Authorization', `Bearer ${token}`)
-//         .send({
-//           email: 'gouda@h.com',
-//           password: '321',
-//         });
-//       expect(res.status).toBe(200);
-//       const { id, email, token: userToken } = res.body.data;
-//       expect(id).toBe(4);
-//       expect(email).toBe('gouda@h.com');
-//       token = userToken;
-//     });
-//   });
-//   describe('Test CRUD API methods', () => {
-//     it('should create new Product item', async () => {
-//       const res = await request
-//         .post('/api/orders')
-//         .set('Content-type', 'application/json')
-//         .send({
-//           status: 'active',
-//           user_id: 4,
-//         });
-//       expect(res.status).toBe(200);
-//       const { status, user_id } = res.body.data;
-//       expect(status).toBe('active');
-//       expect(user_id).toBe('4');
-//     });
-//     it('should get list of orders in the DB', async () => {
-//       const res = await request
-//         .get('/api/orders/')
-//         .set('Content-type', 'application/json')
-//         .set('Authorization', `Bearer ${token}`);
-//       expect(res.status).toBe(200);
-//       expect(res.body.data.orders).not.toBeFalse;
-//     });
-//     it('should get target order info', async () => {
-//       const res = await request
-//         .get('/api/orders/1')
-//         .set('Content-type', 'application/json')
-//         .set('Authorization', `Bearer ${token}`);
-//       expect(res.status).toBe(200);
-//     });
-//     it('should get order info for current user', async () => {
-//       const res = await request
-//         .get('/api/orders/1')
-//         .set('Content-type', 'application/json')
-//         .set('Authorization', `Bearer ${token}`);
-//       expect(res.status).toBe(200);
-//     });
-//     it('should update the target order informations', async () => {
-//       const res = await request
-//         .patch('/api/orders/1')
-//         .set('Content-type', 'application/json')
-//         .set('Authorization', `Bearer ${token}`)
-//         .send({
-//           status: 'active',
-//           user_id: 1,
-//         });
-//       expect(res.status).toBe(200);
-//     });
-//     it('should delete order', async () => {
-//       const res = await request
-//         .delete('/api/orders/1')
-//         .set('Content-type', 'application/json')
-//         .set('Authorization', `Bearer ${token}`);
-//       expect(res.status).toBe(200);
-//     });
-//   });
-// });
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var supertest_1 = __importDefault(require("supertest"));
+var database_1 = __importDefault(require("../../database/database"));
+var server_1 = __importDefault(require("../../server"));
+var user_model_1 = __importDefault(require("../../models/user.model"));
+var userModel = new user_model_1.default();
+var request = (0, supertest_1.default)(server_1.default);
+// eslint-disable-next-line @typescript-eslint/no-inferrable-types
+var token = '';
+describe('Test Orders API Endpoints', function () {
+    beforeAll(function () { return __awaiter(void 0, void 0, void 0, function () {
+        var user;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    user = {
+                        email: 'gouda@h.com',
+                        user_name: 'hos',
+                        first_name: 'Hossam',
+                        last_name: 'Gouda',
+                        password: '321',
+                    };
+                    return [4 /*yield*/, userModel.create(user)];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    }); });
+    afterAll(function () { return __awaiter(void 0, void 0, void 0, function () {
+        var connection, sql;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, database_1.default.connect()];
+                case 1:
+                    connection = _a.sent();
+                    sql = 'DELETE FROM orders;\nALTER SEQUENCE orders_id_seq RESTART WITH 1;\n DELETE FROM users;\nALTER SEQUENCE users_id_seq RESTART WITH 1;';
+                    return [4 /*yield*/, connection.query(sql)];
+                case 2:
+                    _a.sent();
+                    connection.release();
+                    return [2 /*return*/];
+            }
+        });
+    }); });
+    describe('Test Authenticate method', function () {
+        it('should be able to authenticate to get token', function () { return __awaiter(void 0, void 0, void 0, function () {
+            var res, _a, id, email, userToken;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0: return [4 /*yield*/, request
+                            .post('/api/users/auth')
+                            .set('Content-type', 'application/json')
+                            .set('Authorization', "Bearer ".concat(token))
+                            .send({
+                            email: 'gouda@h.com',
+                            password: '321',
+                        })];
+                    case 1:
+                        res = _b.sent();
+                        expect(res.status).toBe(200);
+                        _a = res.body.data, id = _a.id, email = _a.email, userToken = _a.token;
+                        expect(id).toBe(1);
+                        expect(email).toBe('gouda@h.com');
+                        token = userToken;
+                        return [2 /*return*/];
+                }
+            });
+        }); });
+    });
+    describe('Test CRUD API methods', function () {
+        it('should create new Product item', function () { return __awaiter(void 0, void 0, void 0, function () {
+            var res, user_id;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, request
+                            .post('/api/orders')
+                            .set('Content-type', 'application/json')
+                            .send({
+                            status: 'active',
+                            user_id: 1,
+                        })];
+                    case 1:
+                        res = _a.sent();
+                        expect(res.status).toBe(200);
+                        user_id = res.body.data.user_id;
+                        expect(user_id).toBe('1');
+                        return [2 /*return*/];
+                }
+            });
+        }); });
+        it('should get list of orders in the DB', function () { return __awaiter(void 0, void 0, void 0, function () {
+            var res;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, request
+                            .get('/api/orders/')
+                            .set('Content-type', 'application/json')
+                            .set('Authorization', "Bearer ".concat(token))];
+                    case 1:
+                        res = _a.sent();
+                        expect(res.status).toBe(200);
+                        expect(res.body.data.orders).not.toBeFalse;
+                        return [2 /*return*/];
+                }
+            });
+        }); });
+        it('should get target order info', function () { return __awaiter(void 0, void 0, void 0, function () {
+            var res;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, request
+                            .get('/api/orders/1')
+                            .set('Content-type', 'application/json')
+                            .set('Authorization', "Bearer ".concat(token))];
+                    case 1:
+                        res = _a.sent();
+                        expect(res.status).toBe(200);
+                        return [2 /*return*/];
+                }
+            });
+        }); });
+        it('should get order info for current user', function () { return __awaiter(void 0, void 0, void 0, function () {
+            var res;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, request
+                            .get('/api/orders/1')
+                            .set('Content-type', 'application/json')
+                            .set('Authorization', "Bearer ".concat(token))];
+                    case 1:
+                        res = _a.sent();
+                        expect(res.status).toBe(200);
+                        return [2 /*return*/];
+                }
+            });
+        }); });
+        it('should update the target order informations', function () { return __awaiter(void 0, void 0, void 0, function () {
+            var res;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, request
+                            .patch('/api/orders/1')
+                            .set('Content-type', 'application/json')
+                            .set('Authorization', "Bearer ".concat(token))
+                            .send({
+                            status: 'active',
+                            user_id: 1,
+                        })];
+                    case 1:
+                        res = _a.sent();
+                        expect(res.status).toBe(200);
+                        return [2 /*return*/];
+                }
+            });
+        }); });
+        it('should delete order', function () { return __awaiter(void 0, void 0, void 0, function () {
+            var res;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, request
+                            .delete('/api/orders/1')
+                            .set('Content-type', 'application/json')
+                            .set('Authorization', "Bearer ".concat(token))];
+                    case 1:
+                        res = _a.sent();
+                        expect(res.status).toBe(200);
+                        return [2 /*return*/];
+                }
+            });
+        }); });
+    });
+});
